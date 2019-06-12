875c34c8be47e92973a962fac2fa5c5d33aa9896
ftmast <ftmast@gmail.com>
2019-06-12 10:12:50 -0300

    Primeiro commit - Criacao do Projeto

---
+++ src/main/java/com/promocao/promocaoapirest/PromocaApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.promocao.promocaoapirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromocaApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromocaApiRestApplication.class, args);
+	}
+
+}
